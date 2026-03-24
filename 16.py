0ac57424a3ccbd89cf68f613c251596f864e1712
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 11:08:40 -0300

    Add 16.py

---
+++ 16.py
@@ -0,0 +1,17 @@
+# Le o orimeiro numero inteiro
+a = int(input("Digite o valor de a:"))
+
+# Le o segundo numero inteiro
+b = int(input("Digite o valor de b:"))
+
+# Calcule a soma
+soma = a + b
+
+# Calcule a subtração
+subtracao = a - b
+
+# Calcule a mutiplicação
+mutiplicacao = a * b
+
+# Exibe os tres resultado no formato pedido
+print("a + b =", soma, " | a - b =", subtracao," | a * b =", mutiplicacao)
