f159f4b81cfe1be9bee635aa669b17b8373150c2
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 09:47:05 -0300

    Add 10.py

---
+++ 10.py
@@ -0,0 +1,11 @@
+# Valor do raio com numero decimal
+raio = float(input("Digite o raio do círculo:"))
+
+# Define o valor de pi
+pi = 3.14
+
+# Calcule a area do círculo
+area = pi * raio * raio
+
+# Exibe a area calculada
+print("A area é:", area)
