406bb6f42b1668216bff0fa4bd0ca02733d542fc
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 09:08:58 -0300

    Add 07.py

---
+++ 07.py
@@ -0,0 +1,11 @@
+# Primeiro numero decimal
+numero1 = float(input("Digite o primeiro numero decimal:"))
+
+# Segundo numero decimal
+numero2 = float(input("Digite o segundo numero decimal:"))
+
+# Calcule a media dos dois numeros
+media = (numero1 + numero2) / 2
+
+# Calcule a media
+print("A media é:", media)
