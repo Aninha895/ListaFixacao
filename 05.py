3bfbf19629cb61419cfc5cf3dd711f17e4f46996
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 08:34:47 -0300

    Add 05.py

---
+++ 05.py
@@ -0,0 +1,11 @@
+# Le o primeiro numero inteiro 
+numero1 = int(input("Digite o primeiro numero:"))
+
+# Le o segundo numero inteiro
+numero2 = int(input("Digite o segundo numero:"))
+
+# Soma os dois numeros
+soma = numero1 + numero2
+
+#Exibi o resultado da soma
+print("A soma é:", soma)
