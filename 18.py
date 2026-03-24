4d3e904f14f4805886c5da1ae0af4a7e6dad949b
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 12:30:07 -0300

    Add 18.py

---
+++ 18.py
@@ -0,0 +1,8 @@
+# Le o nome do aluno
+nome = input("Digite o nome do aluno:")
+
+# Le a nota do aluno
+nota = float(input("Digite a nota do aluno:"))
+
+# Exibe a frese do formado pedido
+print(f"Aluno {nome} ficou com nota {nota:.1f}")
