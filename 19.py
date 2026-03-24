60583460642bdd0ba548426eaacc889b4b26de6d
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 12:35:16 -0300

    Add 19.py

---
+++ 19.py
@@ -0,0 +1,11 @@
+# Le o ano de nascimento
+ano_nascimeto = int(input("Digite o ano de nascimento:"))
+
+# Digite o ano atual
+ano_atual = 2026
+
+# Calcule a idade estimada
+idade = ano_atual - ano_nascimeto
+
+# Exibe a idade estimada
+print("Idade estimada:", idade)
