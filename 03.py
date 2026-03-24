d35fa10d4f02d25603f21cbfeaa64613506b426c
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 08:20:27 -0300

    Add 03.py

---
+++ 03.py
@@ -0,0 +1,8 @@
+# Digite o nome do usuario
+nome = input("Digite o nome:")
+
+# Digite a cidade do usuario
+cidade = input("Digite a cidade:")
+
+# Exibi o nome e a cidade do usuario
+print(nome, "mora em", cidade + ".")
