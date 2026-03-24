40c4f87fc2fb2adcd9fe43f3d876608fcda9ca1e
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 09:25:17 -0300

    Add 08.py

---
+++ 08.py
@@ -0,0 +1,17 @@
+# Valor digitado pelo usuario com texto
+numero = input("Digite um numero:")
+
+# Exibe o valor lido
+print("Valor lido:", numero)
+
+# Exibe o tipo antes da conversão
+print("Tipo antes:", type(numero))
+
+# Converte o texto para numero inteiro 
+numero =int(numero)
+
+# Exibe o valor depois da conversão
+print("Valor convertido:", numero)
+
+# Exibe o tipo depois da conversão
+print("Tipo depois:", type(numero))
