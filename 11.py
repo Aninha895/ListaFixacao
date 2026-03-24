7c4e44b93987fe9b3b35f7ea4fab72ea6f3f80ea
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 09:57:09 -0300

    Add 11.py

---
+++ 11.py
@@ -0,0 +1,11 @@
+# Le a distancia em quilometros
+distancia = float(input("Digite a distanca em km:"))
+
+# Le o tempo em horas
+tempo = float(input("Digite o tempo em horas:"))
+
+# Calcule a velocidade media
+velocidade_media = distancia / tempo
+
+# Exibe a velocidade media
+print("A velocidade media é:", velocidade_media, "km/h")
