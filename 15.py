d9c856a52e43a9f58622a5579c0b3c73c0a6c63e
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 10:44:47 -0300

    Add 15.py

---
+++ 15.py
@@ -0,0 +1,8 @@
+# Le o nome digitado pelo usuario
+nome = input("Digite o nome:")
+
+# Le a idade dgitado pelo usuario
+idade = input("Digite a idade:")
+
+# Exibe exatamente no formato pedido
+print("Nome:", nome, "| Idade:", idade, "anos")
