0525bda2c06574621b9ee6f5ebd8fa938eab0037
AnaPaulaAlves <anapaula.isa@hotmail.com>
2026-03-24 10:03:50 -0300

    Add 12.py

---
+++ 12.py
@@ -0,0 +1,14 @@
+# Le a primeira nota
+nota1 = float(input("Digite a primeira nota:"))
+
+# Le a segunda nota
+nota2 = float(input("Digite a segunda nota:"))
+
+# Le a terceira nota
+nota3 = float(input("Digite a terceira nota:"))
+
+# Calcule a media das ters notas
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibe a media com duas casa decimal
+print(f"Media: {media: .2f}")
